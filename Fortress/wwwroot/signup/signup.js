e7bf55e2c00df173a975bfa48e8c46d8f0ef79f4
Gabriel Plaza <gabrielplaza@hotmail.com.br>
2023-12-05 00:03:25 -0300

    Validações no formulário de cadastro e ajustes no estilo das páginas

--- Fortress/wwwroot/signup/signup.js
+++ Fortress/wwwroot/signup/signup.js
@@ -1,5 +1,21 @@
 let form = document.getElementById("signup-form")
 
+let password = document.getElementById("signup-password")
+let passwordConfirm = document.getElementById("signup-confirm-password")
+let passwordShow = document.getElementById("signup-show-password")
+
+passwordShow.onchange = (ev) => {
+	password.type = passwordShow.checked ? "text" : "password"
+	passwordConfirm.type = passwordShow.checked ? "text" : "password"
+}
+
+passwordConfirm.onchange = (ev) => {
+	passwordConfirm.setCustomValidity(
+		passwordConfirm.value === password.value
+		? ""
+		: "As senhas digitadas diferem. Tente novamente."
+	)
+}
 
 form.onsubmit = async (ev) => {
 	ev.preventDefault()
